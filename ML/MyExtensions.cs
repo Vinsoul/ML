51bb8dad7c9d91bf2a6cd2d44f5104062267e3fe
Denis.Vinogradov <dv@aihelps.com>
2017-10-06 22:04:47 +0300

    Initial commit

---
+++ ML/MyExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ML
+{
+    static class MyExtensions
+    {
+        public static void ShuffleTrainingSet<T>(this IList<T> input, IList<T> output)
+        {
+            RNGCryptoServiceProvider random = new RNGCryptoServiceProvider();
+
+            int n = input.Count;
+            while (n-- > 0)
+            {
+                byte[] bytes = new byte[4];
+                random.GetBytes(bytes);
+                int j = BitConverter.ToInt32(bytes, 0) % n;
+
+                T buffer = input[n];
+                input[n] = input[j];
+                input[j] = buffer;
+
+                buffer = output[n];
+                output[n] = output[j];
+                output[j] = buffer;
+            }
+        }
+    }
+}
---
+++ ML/Program.cs
@@ -0,0 +1,20 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((new byte[4]) is object);
+        }
+
+
+    }
+}
